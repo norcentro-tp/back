f9d4da99fcdcdccd96d232f52239edd615d027f0
Diego.Carhuavilca <diego.carhuavilca@gmail.com>
2023-05-03 00:15:39 +0000

    Update port for deply

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   const config = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('NORCENTRO api')
@@ -12,6 +13,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
