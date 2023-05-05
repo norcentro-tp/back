aac256df9d666b359152ba07aa0fe8371e44ac1d
Diego Vasquez <diegowal21@hotmail.com>
2023-05-04 19:15:55 -0500

    cambios

--- src/proveedores/proveedores.service.ts
+++ src/proveedores/proveedores.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProveedoreDto } from './dto/create-proveedore.dto';
 import { UpdateProveedoreDto } from './dto/update-proveedore.dto';
+import { Proveedor } from './schemas/proveedores.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ProveedoresService {
+  constructor(@InjectModel(Proveedor.name) private proveedorModel: Model<Proveedor>) {}
   create(createProveedoreDto: CreateProveedoreDto) {
     return 'This action adds a new proveedore';
   }
 
-  findAll() {
-    return `This action returns all proveedores`;
+  async findAll() {
+    return await this.proveedorModel.find();
   }
 
   findOne(id: number) {
